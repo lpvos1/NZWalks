21d3588765cf55554330561238bdbc015f3f54e6
lpvos1 <my.tech@mail.com>
2022-11-03 09:24:02 -0400

    End of Section 4

--- NZWalks/NZWalks.API/Repositories/RegionRepository.cs
+++ NZWalks/NZWalks.API/Repositories/RegionRepository.cs
@@ -6,17 +6,65 @@ namespace NZWalks.API.Repositories
 {
     public class RegionRepository : IRegionRepository
     {
+        private readonly NZWalksDbContext nZWalksDbContext;
 
         public RegionRepository(NZWalksDbContext nZWalksDbContext)
         {
-            this.NZWalksDbContext = nZWalksDbContext;
+            this.nZWalksDbContext = nZWalksDbContext;
         }
 
-        public NZWalksDbContext NZWalksDbContext { get; }
+        public async Task<Region> AddAsync(Region region)
+        {
+            region.Id = Guid.NewGuid();
+            await nZWalksDbContext.AddAsync(region);
+            await nZWalksDbContext.SaveChangesAsync();
+            return region;
+        }
+
+        public async Task<Region> DeleteAsync(Guid id)
+        {
+            var region = await nZWalksDbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(region == null)
+            {
+                return null;
+            }
+
+            //  Delete the region from the database
+            nZWalksDbContext.Regions.Remove(region);
+            await nZWalksDbContext.SaveChangesAsync();
+            return region;
+        }
 
         public async Task<IEnumerable<Region>> GetAllAsync()
         {
-            return await NZWalksDbContext.Regions.ToListAsync();
+            return await nZWalksDbContext.Regions.ToListAsync();
+        }
+
+        public async Task<Region> GetAsync(Guid id)
+        {
+            return await nZWalksDbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Region> UpdateAsync(Guid id, Region region)
+        {
+            var existingRegion = await nZWalksDbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (existingRegion == null)
+            {
+                return null;
+            }
+
+            existingRegion.Code = region.Code;
+            existingRegion.Name = region.Name;
+            existingRegion.Area = region.Area;
+            existingRegion.Lat = region.Lat;
+            existingRegion.Long = region.Long;
+            existingRegion.Population = region.Population;
+
+            await nZWalksDbContext.SaveChangesAsync();
+
+            return existingRegion;
         }
     }
 }
